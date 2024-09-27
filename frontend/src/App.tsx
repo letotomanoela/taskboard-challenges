5da35c7a60483a0dd0b0d5822411764de9bb60a2
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2024-09-27 10:07:17 +0300

    Modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import Task from "./components/Task";
 import Title from "./sections/Title";
+import Modal from "./components/Modal";
 
 const App = () => {
   return (
-    <main className="w-full min-h-screen flex items-center justify-center ">
+    <main className="w-full relative px-6 md:px-0 min-h-screen flex items-center justify-center ">
       <div className="w-[640px] min-h-[300px]">
         <Title />
 
@@ -13,15 +14,24 @@ const App = () => {
           <Task status="WONTDO" title="Task 3" icon="./gif/sad.gif" />
           <Task status="COMPLETED" title="Task 4" icon="./gif/bored.gif" />
         </section>
-
-        <div className="w-full mt-5 bg-[#F5E8D5] flex items-center px-4  h-16 rounded-xl space-x-3 cursor-pointer">
-          <div className="size-10 rounded-xl flex items-center justify-center  bg-[#E9A23B]">
-            <img src="./Add_round_duotone.svg" className="icon" alt="" />
-          </div>
-          <p className="font-semibold">Add Task</p>
-        </div>
+        <AddTaskButton />
       </div>
+
+      <Modal>
+        <div></div>
+      </Modal>
     </main>
   );
 };
 export default App;
+
+const AddTaskButton = () => {
+  return (
+    <div className="w-full mt-5 bg-[#F5E8D5] flex items-center px-4  h-16 rounded-xl space-x-3 cursor-pointer">
+      <div className="size-10 rounded-xl flex items-center justify-center  bg-[#E9A23B]">
+        <img src="./Add_round_duotone.svg" className="icon" alt="" />
+      </div>
+      <p className="font-semibold">Add Task</p>
+    </div>
+  );
+};
