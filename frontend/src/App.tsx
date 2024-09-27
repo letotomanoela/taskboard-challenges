e3c0cd944f612f1f5c5c3bf8e8b4424e628ae125
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2024-09-27 09:56:07 +0300

    Task List

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Task from "./components/Task";
 import Title from "./sections/Title";
 
 const App = () => {
@@ -5,6 +6,20 @@ const App = () => {
     <main className="w-full min-h-screen flex items-center justify-center ">
       <div className="w-[640px] min-h-[300px]">
         <Title />
+
+        <section className="mt-8 space-y-5">
+          <Task status="PROGRESS" title="Task 1" icon="./gif/angry.gif" />
+          <Task status="TODO" title="Task 2" icon="./gif/angry1.gif" />
+          <Task status="WONTDO" title="Task 3" icon="./gif/sad.gif" />
+          <Task status="COMPLETED" title="Task 4" icon="./gif/bored.gif" />
+        </section>
+
+        <div className="w-full mt-5 bg-[#F5E8D5] flex items-center px-4  h-16 rounded-xl space-x-3 cursor-pointer">
+          <div className="size-10 rounded-xl flex items-center justify-center  bg-[#E9A23B]">
+            <img src="./Add_round_duotone.svg" className="icon" alt="" />
+          </div>
+          <p className="font-semibold">Add Task</p>
+        </div>
       </div>
     </main>
   );
---
+++ frontend/src/components/Task.tsx
@@ -0,0 +1,52 @@
+import clsx from "clsx";
+
+type Props = {
+  status: "PROGRESS" | "TODO" | "WONTDO" | "COMPLETED";
+  title: string;
+  description?: string;
+  icon?: string;
+};
+
+const Task = ({ status, title, description, icon }: Props) => {
+  return (
+    <div
+      className={clsx(
+        "w-full min-h-16    rounded-2xl grid grid-cols-[40px,1fr,40px]  px-4",
+        status === "PROGRESS" && "bg-[#F5D565]",
+        status === "COMPLETED" && "bg-[#A0ECB1]",
+        status === "WONTDO" && "bg-[#F7D4D3]",
+        status === "TODO" && "bg-[#E3E8EF]",
+        description ? "items-start pt-2" : "items-center"
+      )}
+    >
+      <div className="size-10 flex items-center justify-center bg-white rounded-xl">
+        <img src={icon} className="w-8" alt="" />
+      </div>
+      <p className="text-black  text-lg pl-3">
+        <p className="font-bold">{title}</p>
+        <p className="text-sm">{description}</p>
+      </p>
+      <div
+        className={clsx(
+          "size-10 bg-white rounded-xl flex items-center justify-center",
+
+          status === "TODO" && "opacity-0",
+          status === "PROGRESS" && "bg-[#E9A23B]",
+          status === "COMPLETED" && "bg-[#32D657]",
+          status === "WONTDO" && "bg-[#DD524C]"
+        )}
+      >
+        <img
+          src={clsx(
+            status === "COMPLETED" && "./Done_round_duotone.svg",
+            status === "WONTDO" && "./close_ring_duotone.svg",
+            status === "PROGRESS" && "./Time_atack_duotone.svg"
+          )}
+          className="icon"
+          alt="icon"
+        />
+      </div>
+    </div>
+  );
+};
+export default Task;
